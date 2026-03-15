1f5e2be224fc9e521beaeecb6840b09e772dabf4
Al-hadi.co <53935711+alhaditraining@users.noreply.github.com>
2026-03-15 13:30:41 +0300

    Add setuptools configuration file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="erpnext_law_ui",
+    version="0.0.1",
+    description="UI Application for Law Management System",
+    author="Alhadi Training",
+    author_email="info@alhaditraining.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[]
+)
